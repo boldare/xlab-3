4498abb2e16343c7dcd2331017aabee0e6facc7e
vagrant <vagrant@xsworkshops.dev>
2016-07-13 12:38:53 +0200

    2 - legacy example with tests

---
+++ src/XLab/Dependencies/Database/Legacy/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once implode(DIRECTORY_SEPARATOR, [
+    __DIR__,
+    '..',
+    '..',
+    '..',
+    '..',
+    '..',
+    'vendor',
+    'autoload.php',
+]);
+
+$repository = new XLab\Dependencies\Database\Legacy\UserRepository(__DIR__ . '/../db.sqlite');
+$user = $repository->find(1);
+
+var_dump($user);
+
+$user->setAvatarPath(__DIR__ .'/../../API/avatars/1.png');
+$repository->save($user);
+
+var_dump($repository->find(1));
+
+$user->setAvatarPath();
+$repository->save($user);
+
+var_dump($repository->find(1));
