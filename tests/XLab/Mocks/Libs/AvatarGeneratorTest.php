a9b829838e49ccd2e095ee3fc01c3c16bc7291a1
vagrant <vagrant@xsworkshops.dev>
2016-07-15 17:11:34 +0200

    3 - examples of mocking with different libraries

---
+++ tests/XLab/Mocks/Libs/AvatarGeneratorTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace XLab\Mocks\Libs\Tests;
+
+use XLab\Dependencies\API\Proper\Avatar;
+use XLab\Dependencies\API\Proper\AvatarGenerator;
+use XLab\Dependencies\API\Proper\AvatarRetrieverInterface;
+use XLab\Dependencies\API\Proper\AvatarStorageInterface;
+use XLab\Dependencies\API\User;
+
+class AvatarGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGeneratePHPUnitMockObject()
+    {
+        $retriever = $this->getMockBuilder(AvatarRetrieverInterface::class)->getMock();
+        $storage = $this->getMockBuilder(AvatarStorageInterface::class)->getMock();
+        $generator = new AvatarGenerator($retriever, $storage);
+
+        $user = new User(123, 'user@example.com');
+        $avatar = new Avatar('avatar name', 'avatar content');
+
+        $retriever->expects($this->once())
+            ->method('getAvatar')
+            ->with($user)
+            ->willReturn($avatar);
+        $storage->expects($this->once())
+            ->method('store')
+            ->with($avatar)
+            ->willReturn('filepath');
+
+        $this->assertEquals('filepath', $generator->generate($user));
+        $this->assertEquals('filepath', $user->getAvatarPath());
+    }
+
+    public function testGenerateProphecy()
+    {
+        $retriever = $this->prophesize(AvatarRetrieverInterface::class);
+        $storage = $this->prophesize(AvatarStorageInterface::class);
+        $generator = new AvatarGenerator($retriever->reveal(), $storage->reveal());
+
+        $user = new User(123, 'user@example.com');
+        $avatar = new Avatar('avatar name', 'avatar content');
+
+        $retriever->getAvatar($user)->willReturn($avatar);
+        $storage->store($avatar)->willReturn('filepath');
+
+        $this->assertEquals('filepath', $generator->generate($user));
+        $this->assertEquals('filepath', $user->getAvatarPath());
+    }
+
+    public function testGenerateMockery()
+    {
+        $user = new User(123, 'user@example.com');
+        $avatar = new Avatar('avatar name', 'avatar content');
+
+        $retriever = \Mockery::mock(AvatarRetrieverInterface::class)
+            ->shouldReceive('getAvatar')
+            ->with($user)
+            ->andReturn($avatar)
+            ->mock();
+        $storage = \Mockery::mock(AvatarStorageInterface::class)
+            ->shouldReceive('store')
+            ->with($avatar)
+            ->andReturn('filepath')
+            ->mock();
+        $generator = new AvatarGenerator($retriever, $storage);
+
+        $this->assertEquals('filepath', $generator->generate($user));
+        $this->assertEquals('filepath', $user->getAvatarPath());
+
+        \Mockery::close();
+    }
+
+    public function testGeneratePhake()
+    {
+        $retriever = \Phake::mock(AvatarRetrieverInterface::class);
+        $storage = \Phake::mock(AvatarStorageInterface::class);
+        $generator = new AvatarGenerator($retriever, $storage);
+
+        $user = new User(123, 'user@example.com');
+        $avatar = new Avatar('avatar name', 'avatar content');
+
+        \Phake::when($retriever)->getAvatar($user)->thenReturn($avatar);
+        \Phake::when($storage)->store($avatar)->thenReturn('filepath');
+
+        $this->assertEquals('filepath', $generator->generate($user));
+        $this->assertEquals('filepath', $user->getAvatarPath());
+    }
+}
